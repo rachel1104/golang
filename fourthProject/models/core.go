82fe0d01b302165c7b752c9f07c6715cb2f71749
rachel1104 <52815040+rachel1104@users.noreply.github.com>
2024-12-27 22:15:33 +0800

    Update core.go

--- fourthProject/models/core.go
+++ fourthProject/models/core.go
@@ -11,7 +11,7 @@ var err error
 
 func init() {
 	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
-	dsn := "test01:chenchen1234@tcp(127.0.0.1:3306)/gin?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "test01:123456@tcp(127.0.0.1:3306)/gin?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
 		//禁用事務 性能提升30%
 		SkipDefaultTransaction: true,
